40c741f92a5b11f4532df5ce3a4188a3807bbd1c
OleksandrNarikov <narikovolexandr@gmail.com>
2026-05-13 00:59:43 +0300

    Better dialogue

--- dialogue/game_dialogue_balloon.gd
+++ dialogue/game_dialogue_balloon.gd
@@ -1,1 +1,10 @@
 extends BaseGameDialogueBaloon
+@onready var emotes_panel: Panel = $Balloon/MarginContainer/PanelContainer/MarginContainer/HBoxContainer/EmotesPanel
+
+func start(with_dialogue_resource: DialogueResource = null, title: String = "", extra_game_states: Array = []) -> void:
+	super.start(with_dialogue_resource, title, extra_game_states)
+	emotes_panel.play_emote("emote_12_talking")
+
+func next(next_id: String) -> void:
+	super.next(next_id)
+	emotes_panel.play_emote("emote_12_talking")
